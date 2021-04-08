2ebb66effb7c8407fdd2ccfc7fae273c71698578
Xvaug <Xvaug@XCV>
2021-04-07 22:58:59 -0500

    Assignment 6 2.0

---
+++ large-scale-programming/src/org/howard/edu/lsp/assignment6/IntegerSet.java
@@ -0,0 +1,227 @@
+package org.howard.edu.lsp.assignment6;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+//import org.howard.edu.lsp.assignment4.IntegerSet;
+import org.howard.edu.lsp.assignment4.IntegerSetException;
+
+class IntegerSet{
+	//Creates a new Arraylist called set
+	private ArrayList<Integer> set = new ArrayList<Integer>();
+	
+	// clears the ArraytList of all items in the said ArrayList
+	public void clear()
+	{
+		set.clear();
+	}
+	
+	//Gets the length of the ArrayList and returns it 
+	public int length()
+	{
+		return set.size();
+	}
+	
+	//Checks to see if the sets, or ArrayLists contain the same numbers
+	public boolean equals(IntegerSet b)
+	{
+		ArrayList<Integer> setCompare1 = new ArrayList<Integer>();
+		ArrayList<Integer> setCompare2 = new ArrayList<Integer>();
+		
+		setCompare1 = set;
+		setCompare2 = b.set;
+		
+		boolean equal = false;
+		if (set.size() != b.length())
+		{
+			equal = false;
+		}
+		else if(set.size() == b.length())
+		{
+			Collections.sort(setCompare1);
+			Collections.sort(setCompare2);
+			
+			equal = setCompare1.equals(setCompare2);
+			
+		}
+		return equal;
+	}
+	
+	//Check to see if the Set contains a certain number value
+	public boolean contains(int value)
+	{
+		boolean b;
+		
+		if (set.contains(value))
+		{
+			b = true;
+		}
+		else
+		{
+			b = false;
+		}
+		
+		
+		return b;
+	}
+	
+	// gets the largest int in the set
+	public int largest() throws IntegerSetException
+	{
+	
+	int largest;
+
+	if (set.isEmpty())
+	{
+		throw new IntegerSetException("The String is Empty, so largest int cannot be found");
+	}
+	
+	
+	else
+	{
+		largest = set.get(0);
+		for (int i = 0; i < set.size(); i++)
+		{
+			if (largest < set.get(i))
+			{
+				largest = set.get(i);
+			}
+			else
+			{
+				continue;
+			}
+		}
+	}
+	return largest;
+	}
+	
+	//gets the smallest int in the set
+	public int smallest() throws IntegerSetException
+	{
+		int smallest;
+		
+		if (set.isEmpty())
+		{
+			throw new IntegerSetException("The String is Empty, so smallest int cannot be found");
+		}
+		
+		else
+		{
+			smallest = set.get(0);
+			for (int i = 0; i < set.size(); i++)
+			{
+				if (smallest > set.get(i))
+				{
+					smallest = set.get(i);
+				}
+				else
+				{
+					continue;
+				}
+			}
+		}
+		return smallest;
+	}
+	
+	//adds an integer to the set if it is not already in the set
+	public void add(int item) 
+	{
+		if (set.contains(item) != true)
+		{
+		set.add(item);
+		}
+		else
+		{
+			
+		}
+		return;
+	}
+	
+	//removes an integer from the set if its in the set
+	public void remove(int item)
+	{
+		if (set.contains(item) == true)
+		{
+			for (int i=0; i < set.size(); i++)
+			{
+				if(set.get(i) == item)
+				{
+					set.remove(i);
+				}
+				else
+				{
+					continue;
+				}
+		
+			}
+		}
+		else
+		{
+		
+		}
+		return;
+	}
+	
+	//combines two sets to make a new set with all the items from both sets
+	public void union(IntegerSet intSetb)
+	{
+		IntegerSet set3 = new IntegerSet();
+		String S3;
+	for(int i= 0; i < set.size() ; i++)
+	{
+		set3.add(set.get(i));
+		
+	}
+	for(int j= 0; j < intSetb.length() ; j++)
+	{
+		set3.add(intSetb.set.get(j));
+		
+	}
+	
+	
+	S3 = set3.set.toString();
+	
+	System.out.println(S3);
+	}
+	
+	// Prints out where the sets share numbers.
+	public void intersect(IntegerSet intSetb) 
+	{
+		String S4;
+		IntegerSet set4 = new IntegerSet();
+		
+		set4.set.addAll(set);
+		set4.set.retainAll(intSetb.set);
+		
+		S4 = set4.set.toString();
+		
+		System.out.println(S4);
+	}
+	
+	// Sees if the set is empty or not
+	boolean isEmpty()
+	{
+		return set.isEmpty();
+	}
+	
+	// gets and prints out the numbers that do not overlap in the sets;
+	public void diff(IntegerSet intSetb)
+	{
+		String S5;
+		IntegerSet set5 = new IntegerSet();
+		
+		set5.set.addAll(set);
+		
+		set5.set.removeAll(intSetb.set);
+		
+		S5 = set5.set.toString();
+		
+		System.out.println(S5);
+		
+	}
+	
+	public String toString()
+	{
+		return set.toString();
+	}
+	}
---
+++ large-scale-programming/src/org/howard/edu/lsp/assignment6/IntegerSetTest.java
@@ -0,0 +1,268 @@
+package org.howard.edu.lsp.assignment6;
+
+import static org.junit.jupiter.api.Assertions.*;
+//import IntegerSet.Java;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+
+import org.howard.edu.lsp.assignment4.IntegerSetException;
+import org.howard.edu.lsp.assignment6.IntegerSet;
+import org.junit.jupiter.api.DisplayName;
+
+class IntegerSetTest {
+
+	 @Test
+	 @DisplayName("Test cases for add")
+	 public void testAdd() 
+	 {
+	   IntegerSet test = new IntegerSet();
+	   
+	   test.add(1);
+	   
+   	assertEquals("[1]", test.toString());
+
+
+	 }
+	 
+	 @Test
+	 @DisplayName("Test case for contains the number 7")
+	 public void testContains() 
+	 {
+	   IntegerSet test = new IntegerSet();
+	   boolean contains;
+	   
+	   test.add(1);
+	   test.add(5);
+	   test.add(7);
+	   test.add(9);
+	   test.add(2);
+	   
+	   
+	   contains = test.contains(7);
+	   
+	   assertEquals(true, contains);
+
+
+	 }
+	 //Junit case checks if the Integer Set contains the number 7
+	    	
+    @Test
+    @DisplayName("Test cases for equals")
+    public void testEquals() {
+    	
+    	boolean isEqual;
+    	
+    	IntegerSet test = new IntegerSet();
+    	//Checks the equal function
+    	test.add(9);
+    	test.add(10);
+    	test.add(11);
+    	test.add(12);
+    	//System.out.println("Set 5: " + testSet5.toString());
+    	
+    	IntegerSet test2 = new IntegerSet();
+    	test2.add(9);
+    	test2.add(10);
+    	test2.add(11);
+    	test2.add(12);
+    	//System.out.println("Set 6: " + testSet6.toString());
+    	
+    	//Should be same as they are the exact same
+    	isEqual = test.equals(test2);
+    	
+    	assertEquals(true, isEqual);
+    	
+    	IntegerSet test3 = new IntegerSet();
+    	test3.add(10);
+    	test3.add(9);
+    	test3.add(12);
+    	test3.add(11);
+    	//System.out.println("Set 7: " + testSet7.toString());
+    	
+    	//Should be same even though they are out of order
+    	isEqual = test.equals(test3);
+    	
+    	assertEquals(true, isEqual);
+    	
+    	IntegerSet test4 = new IntegerSet();
+    	test4.add(9);
+    	test4.add(10);
+    	test4.add(11);
+    	test4.add(12);
+    	test4.add(13);
+    	//System.out.println("Set 8: " + testSet8.toString());
+    	//Should be different as a new number is introduced.
+    	isEqual = test3.equals(test4);
+    	
+    	assertEquals(false, isEqual);	
+    }
+ // Junit test case for testing equals method
+    
+    @Test
+    @DisplayName("Test cases for toString")
+    public void testtoString() {
+    String actualString;
+    // The expected String
+    
+    IntegerSet test = new IntegerSet();
+	test.add(1);
+	test.add(2);
+	test.add(3);
+	//Checks to see if the numbers were properly added, as well as if the toString method works
+	actualString = test.toString();
+	
+	assertEquals("[1, 2, 3]", actualString);
+	}
+ // Junit test case for testing toString method
+    
+    @Test
+    @DisplayName("Test cases for remove")
+    public void testRemove() {
+    String actualString;
+    IntegerSet test = new IntegerSet();
+   	test.add(1);
+   	test.add(2);
+   	test.add(3);
+   	test.add(4);
+   	
+    test.remove(4);
+    
+    actualString = test.toString();
+    
+    assertEquals("[1, 2, 3]", actualString);
+    
+    }
+ // Junit test case for testing remove method
+    
+    @Test
+    @DisplayName("Test cases for clear")
+    public void testClear() {
+    String actualString;
+    IntegerSet test = new IntegerSet();
+    
+   	test.add(1);
+   	test.add(2);
+   	test.add(3);
+   	
+    test.clear();
+    
+    actualString = test.toString();
+    
+    assertEquals("[]", actualString);
+    
+    }
+ // Junit test case for testing the clear method
+    
+    @Test
+    @DisplayName("Test cases for largest")
+    public void testLargest() throws IntegerSetException {
+    int largestNum;
+    IntegerSet test = new IntegerSet();
+   	test.add(1);
+   	test.add(2);
+   	test.add(3);
+   	test.add(4);
+   	
+    largestNum = test.largest();
+    
+    
+    assertEquals(4, largestNum);
+    
+    }
+ // Junit test case for testing the largest value in a set
+    
+    /*
+    @Test
+    @DisplayName("Test cases for largest value exception")
+    public void testLargestException() throws IntegerSetException {
+    int largestNum;
+    IntegerSet test = new IntegerSet();
+   	test.add(1);
+   	test.add(2);
+   	test.add(3);
+   	test.add(4);
+   	
+   	test.clear();
+    largestNum = test.largest();
+    
+    
+    assertThrows(string , largestNum);
+    
+    }
+    */
+ // Junit test case for testing the exception in the largest value class
+    
+    @Test
+    @DisplayName("Test cases for smallest value")
+    public void testSmallest() throws IntegerSetException {
+    int smallestNum;
+    IntegerSet test = new IntegerSet();
+   	test.add(1);
+   	test.add(2);
+   	test.add(3);
+   	test.add(4);
+   	
+   	
+    smallestNum = test.smallest();
+    
+    
+    assertEquals(1,smallestNum);
+    }
+ // Junit test case for testing smallest int in a set
+    
+    @Test
+    @DisplayName("Test case for isEmpty class, not empty")
+    public void testIsEmpty() throws IntegerSetException {
+    boolean testContains;
+    IntegerSet test = new IntegerSet();
+   	test.add(1);
+   	test.add(2);
+   	test.add(3);
+   	test.add(4);
+   	
+   	
+    testContains = test.isEmpty();
+    
+    
+    assertEquals(false ,testContains);
+    }
+ // Junit test case for testing a non empty Integer Set
+
+    @Test
+    @DisplayName("Test case for isEmpty class, empty")
+    public void testIsEmpty2() throws IntegerSetException {
+    boolean testContains;
+    IntegerSet test = new IntegerSet();
+   	
+   	
+    testContains = test.isEmpty();
+    
+    
+    assertEquals(true ,testContains);
+    }
+    // Junit test case for testing an empty integerset
+
+    @Test
+    @DisplayName("Test case for length")
+    public void testLength() throws IntegerSetException {
+    int length;
+    
+    IntegerSet test = new IntegerSet();
+   	test.add(1);
+   	test.add(2);
+   	test.add(3);
+   	test.add(4);
+   	test.add(5);
+   	
+   	
+    length = test.length();
+    
+    
+    assertEquals(5,length);
+    }
+      // Junit test case for testing length
+    }
+
+
+
+
